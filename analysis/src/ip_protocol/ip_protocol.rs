90b05d0309fb1b093d093eff0ba7c0fb6d5e80bd
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2024-10-08 15:16:37 +0200

    Clean up

--- analysis/src/ip_protocol/ip_protocol.rs
+++ analysis/src/ip_protocol/ip_protocol.rs
@@ -1,13 +1,14 @@
 use std::any::Any;
 
-// use pcap::Packet;
 use crate::ethernet_frame::EthernetFrame;
 
 pub trait Packet {
     fn serialize(packet: &pcap::Packet, eth_frame: EthernetFrame) -> Box<dyn Packet>
     where
         Self: Sized;
+
+    #[allow(dead_code)]
     fn get_eth_frame(&self) -> &EthernetFrame;
+    
     fn as_any(&self) -> &dyn Any;
 }
-
